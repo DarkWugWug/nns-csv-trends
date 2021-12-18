93742e45210376af8131a206b37f9c33f92a710f
“Nolan <nolanrosen15@gmail.com>
2021-12-17 19:53:42 -0500

    Ahhh yes complexity grows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::fs::{ OpenOptions, File };
-use std::io::BufReader;
+use std::io::{ BufReader, Lines };
 use std::io::prelude::*;
+use std::str::FromStr;
+use std::collections::{ HashMap };
+use std::collections::hash_map::{ Entry, OccupiedEntry, VacantEntry };
+use rust_decimal::Decimal;
 use clap::{ Arg, App };
-use diligent_date_parser::parse_date;
 
 mod error;
 use error::Error;
@@ -17,22 +20,45 @@ fn main() {
             .required(true)
             .index(1)
         ).get_matches();
-    
     let file_name = args.value_of("INPUT").unwrap();
     let reader = match open_file(file_name) {
         Ok(f) => f,
         Err(e) => return eprintln!("{}", e)
     };
-    for line in reader.lines().map(|l| l.unwrap()) {
-        // TODO: Add custom delimiter support
-        let item_iter = line.split(",");
-        for item in item_iter {
-            // dbg!(item);
-            if let Some(date) = parse_date(item) {
-                println!("Found Date: {}", date);
-            }
+    // for line in reader.lines().map(|l| l.unwrap()) {
+    //     // TODO: Add custom delimiter support
+    //     let item_iter = line.split(",");
+    //     for item in item_iter {
+    //         if let Some(date) = parse_date(item) {
+    //             println!("Found Date: {}", date);
+    //         }
+    //     }
+    // }
+    let mut reader = reader.lines();
+    reader.next();
+    let dup_amounts = find_duplicates(reader, 3);
+    for (k, i) in dup_amounts {
+        if i > 1 {
+            println!("Reoccuring amount of `{}` seen {} times", k, i);
         }
     }
+}
+
+fn find_duplicates(reader: Lines<BufReader<File>>, col: usize) -> HashMap<Decimal, usize> {
+    let mut hasmap: HashMap<Decimal, usize> = HashMap::new();
+    let iter = reader
+        .map(|l| l.unwrap())
+        .filter_map(|l| l.split(",")
+            .nth(col)
+            .and_then(|i| Some(i.to_string()))
+        ).map(|ref i| Decimal::from_str(i).unwrap());
+    for item in iter {
+        match hasmap.entry(item) {
+            Entry::Occupied(mut e) =>  { let _ = e.insert(e.get() + 1); },
+            Entry::Vacant(e) => { let _ = e.insert(1); }
+        }
+    }
+    return hasmap
 }
 
 fn open_file(file_name: &str) -> Result<BufReader<File>, Error> {
